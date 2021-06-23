e66a71b45255215d7039c8c1ee5c88a5d193530f
jihoon <wlgns2223@gmail.com>
2021-06-23 18:26:46 +0900

    navbar complete

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
+'use strict'
+
+
+// navbar 투명하게, 높이를 줄임
 window.addEventListener('scroll',(e)=>{
     const currentScroll = window.scrollY;
-    const val = 100
+    const $mynav = document.querySelector('.mynav');
+    const navHeight = $mynav.getBoundingClientRect().height;
     const $header = document.querySelector('.myheader');
 
-    if(currentScroll >= val){
-        $header.style.backgroundColor = '#3dbab2';
+    if(currentScroll >= navHeight){
+        $mynav.classList.add('mynav--short');
+        $header.classList.add('myheader--dark');
+
+
     } else {
-        $header.style.backgroundColor = 'transparent';
+        $header.classList.remove('myheader--dark');
+        $mynav.classList.remove('mynav--short');
+
     }
 });
